a0bc05aeb8d496a30938c4f1ad6f4b6e6137f09a
samerlahoud <samer@lahoud.fr>
2019-03-04 11:28:40 +0200

    tbeam lorawan

--- ttgo-tbeam-lorawan-otaa/ttgo-tbeam-lorawan-otaa.ino
+++ ttgo-tbeam-lorawan-otaa/ttgo-tbeam-lorawan-otaa.ino
@@ -14,32 +14,22 @@
 #include <hal/hal.h>
 #include <SPI.h>
 
-#include <SSD1306.h>
-
 #define LEDPIN 2
-
-// Screen configuration
-#define OLED_I2C_ADDR 0x3C
-#define OLED_RESET 16
-#define OLED_SDA 21
-#define OLED_SCL 22
 
 unsigned int counter = 0;
 char ns_response[30];
 
-SSD1306 display (OLED_I2C_ADDR, OLED_SDA, OLED_SCL);
-
 // This EUI must be in little-endian format, so least-significant-byte
 // first. When copying an EUI from loraserver, this means to reverse
 // the bytes.
-static const u1_t PROGMEM DEVEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};   // LSB mode
+static const u1_t PROGMEM DEVEUI[8]={ 0x23, 0xbd, 0x73, 0x86, 0xd8, 0x5d, 0xd4, 0xd5};   // LSB mode
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from loraserver can be copied as-is.
-
-static const u1_t PROGMEM APPKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // MSB mode
+         
+static const u1_t PROGMEM APPKEY[16] = { 0x95, 0xc1, 0x9a, 0xc0, 0xae, 0xaf, 0xef, 0xdc, 0x3a, 0xbd, 0x6f, 0xef, 0xcc, 0xbd, 0x4f, 0xb3 }; // MSB mode
 void os_getDevKey (u1_t* buf) { memcpy_P(buf, APPKEY, 16);}
 
 // This key is not used anymore, it can be left as all zeros.
@@ -74,10 +64,6 @@ void do_send(osjob_t* j){
         LMIC_setTxData2(1, message, sizeof(message)-1, 0);
         Serial.println(F("Sending uplink packet..."));
         digitalWrite(LEDPIN, HIGH);
-        display.clear();
-        display.drawString (0, 0, "Sending uplink packet...");
-        display.drawString (0, 50, String (++counter));
-        display.display ();
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
@@ -88,12 +74,9 @@ void onEvent (ev_t ev) {
     switch(ev) {
            case EV_TXCOMPLETE:
             Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            display.clear();
-            display.drawString (0, 0, "EV_TXCOMPLETE event!");
 
             if (LMIC.txrxFlags & TXRX_ACK) {
               Serial.println(F("Received ack"));
-              display.drawString (0, 20, "Received ACK.");
             }
 
             if (LMIC.dataLen) {
@@ -104,33 +87,22 @@ void onEvent (ev_t ev) {
               Serial.println();
               Serial.println(LMIC.rssi);
 
-              display.drawString (0, 9, "Received DATA.");
               for ( i = 0 ; i < LMIC.dataLen ; i++ )
                 ns_response[i] = LMIC.frame[LMIC.dataBeg+i];
               ns_response[i] = 0;
-              display.drawString (0, 22, String(ns_response));
-              display.drawString (0, 32, String(LMIC.rssi));
-              display.drawString (64,32, String(LMIC.snr));
             }
 
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             digitalWrite(LEDPIN, LOW);
-            display.drawString (0, 50, String (counter));
-            display.display ();
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_JOINING:
             Serial.println(F("EV_JOINING: -> Joining..."));
-            display.drawString(0,16 , "OTAA joining....");
-            display.display();
             break;
         case EV_JOINED: {
               Serial.println(F("EV_JOINED"));
-              display.clear();
-              display.drawString(0 , 0 ,  "Joined!");
-              display.display();
               // Disable link check validation (automatically enabled
               // during join, but not supported by TTN at this time).
               LMIC_setLinkCheckMode(0);
@@ -160,21 +132,6 @@ void setup() {
 
     // Use the Blue pin to signal transmission.
     pinMode(LEDPIN,OUTPUT);
-
-    //Set up and reset the OLED
-    pinMode(OLED_RESET, OUTPUT);
-    digitalWrite(OLED_RESET, LOW);
-    delay(50);
-    digitalWrite(OLED_RESET, HIGH);
-
-    display.init ();
-    display.flipScreenVertically ();
-    display.setFont (ArialMT_Plain_10);
-
-    display.setTextAlignment (TEXT_ALIGN_LEFT);
-
-    display.drawString (0, 0, "Starting....");
-    display.display ();
 
     // LMIC init
     os_init();
